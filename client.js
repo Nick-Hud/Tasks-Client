99f7ba49d609ca6c7b953cad2dde348e51559b0c
Nick Hudson <njhudson25@gmail.com>
2023-03-28 23:30:20 +0100

    Added authentication

--- client.js
+++ client.js
@@ -1,19 +1,26 @@
-const ws = new WebSocket('wss://wss.njhudson.co.uk:8082');
+let URL = 'wss://invalid.com'
+let ws = new WebSocket(URL);
+function connect() {
+  console.log("Submit")
+  let usernameValue = document.getElementById("username").value;
+  const passwordValue = document.getElementById("password").value;
+  URL = 'wss://wss.njhudson.co.uk:8082/?username=' + usernameValue + '&password=' + passwordValue
+  ws = new WebSocket(URL);
 
-ws.onopen = function() {
-  console.log('Connected to WebSocket server');
-  ws.send('Hello, server!');
-};
+  ws.onopen = function () {
+    console.log('Connected to WebSocket server');
+    ws.send('Hello, server!');
+  };
+  ws.onmessage = function (event) {
+    console.log('received: ' + event.data);
+  };
+  
+  ws.onerror = function (event) {
+    console.error('WebSocket error: ' + event);
+  };
+  
+  ws.onclose = function () {
+    console.log('WebSocket closed');
+  };
 
-// Log incoming messages from server
-ws.onmessage = function(event) {
-  console.log('received: ' + event.data);
-};
-
-ws.onerror = function(event) {
-  console.error('WebSocket error: ' + event);
-};
-
-ws.onclose = function() {
-  console.log('WebSocket closed');
-};
+}
