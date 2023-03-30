cd8936786210d0a462cf94c17c4e3bc8c8451871
Nick Hudson <njhudson25@gmail.com>
2023-03-30 21:36:54 +0100

    Did some doc id shit so it displays connection

--- client.js
+++ client.js
@@ -3,24 +3,26 @@ let ws = new WebSocket(URL);
 function connect() {
   console.log("Submit")
   let usernameValue = document.getElementById("username").value;
-  const passwordValue = document.getElementById("password").value;
+  let passwordValue = document.getElementById("password").value;
   URL = 'wss://wss.njhudson.co.uk:8082/?username=' + usernameValue + '&password=' + passwordValue
   ws = new WebSocket(URL);
 
   ws.onopen = function () {
     console.log('Connected to WebSocket server');
-    ws.send('Hello, server!');
+    document.getElementById("connectedStatus").innerHTML = "Connected"
+    ws.send('clientWelcome');
   };
   ws.onmessage = function (event) {
     console.log('received: ' + event.data);
   };
-  
+
   ws.onerror = function (event) {
     console.error('WebSocket error: ' + event);
   };
-  
+
   ws.onclose = function () {
     console.log('WebSocket closed');
+    document.getElementById("connectedStatus").innerHTML = "Not Connected"
   };
 
 }
