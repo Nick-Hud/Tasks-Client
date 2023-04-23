4867aacee3be23fa5ad6de97b42453d56a767e9b
Nick Hudson <njhudson25@gmail.com>
2023-04-23 19:49:44 +0100

    Auth working

--- client.js
+++ client.js
@@ -1,31 +1,30 @@
-let URL = 'wss://invalid.com'
+let URL = 'wss://wss.njhudson.co.uk:8082'
 let ws = new WebSocket(URL);
-function connect() {
+ws.onopen = function () {
+  console.log('Connected to WebSocket server');
+  document.getElementById("connectedStatus").innerHTML = "Connected"
+  ws.send('clientWelcome');
+};
+ws.onmessage = function (event) {
+  console.log('received: ' + event.data);
+  getData(String(event.data))
+};
+
+ws.onerror = function (event) {
+  console.error('WebSocket error: ' + event);
+};
+
+ws.onclose = function () {
+  console.log('WebSocket closed');
+  document.getElementById("connectedStatus").innerHTML = "Not Connected"
+};
+
+function submitCreds() {
   console.log("Submit")
   let usernameValue = document.getElementById("username").value;
   let passwordValue = document.getElementById("password").value;
-  URL = 'wss://wss.njhudson.co.uk:8082/?username=' + usernameValue + '&password=' + passwordValue
-  ws = new WebSocket(URL);
-
-  ws.onopen = function () {
-    console.log('Connected to WebSocket server');
-    document.getElementById("connectedStatus").innerHTML = "Connected"
-    ws.send('clientWelcome');
-    ws.send("usrnme:" + usernameValue)
-  };
-  ws.onmessage = function (event) {
-    console.log('received: ' + event.data);
-    getData(String(event.data))
-  };
-
-  ws.onerror = function (event) {
-    console.error('WebSocket error: ' + event);
-  };
-
-  ws.onclose = function () {
-    console.log('WebSocket closed');
-    document.getElementById("connectedStatus").innerHTML = "Not Connected"
-  };
+  ws.send("usrnme:" + usernameValue)
+  ws.send("pass:" + passwordValue)
 
 }
 
